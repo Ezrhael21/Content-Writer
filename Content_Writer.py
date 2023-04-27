4ce64b77935afdecec23eb7fc9f3e1cef0ddc88a
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-04-28 01:38:42 +0800

    Pseudocode

---
+++ Content_Writer.py
@@ -0,0 +1,11 @@
+# Ezrhael R. Sicat
+# BSCpE 1-5
+# 04/27/2023
+# Program 3: Write a text into a file
+
+# open a mylife text file
+# create a loop for y/n
+# ask the user to enter a line
+# ask the user y/n for more lines
+# write the user text and user line in mylife file
+# if no break the loop
