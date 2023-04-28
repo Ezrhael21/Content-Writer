3a885655b413eed22cad34c84f0a22cd80ad4da4
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-04-28 22:38:04 +0800

    Import Colorama

--- Content_Writer.py
+++ Content_Writer.py
@@ -4,25 +4,26 @@
 # Program 3: Write a text into a file
 
 import pyfiglet
+import colorama
 
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
 font = pyfiglet.figlet_format("Writing a text into a file", font = "big", justify = "center")
-print (font)
+print (colorama.Fore.YELLOW + font)
 
 # Introduction to the Program 
-print ("=" * 100)
-user_name = input("Enter your username: ")
-print ("=" * 100)
-print ("Hello!", user_name)
-print ("Today, We are going to Write a text into a file")
+print (colorama.Fore.GREEN + "=" * 100)
+user_name = input(colorama.Fore.BLUE + "Enter your username: ")
+print (colorama.Fore.GREEN + "=" * 100)
+print (colorama.Fore.WHITE + "Hello!", colorama.Fore.YELLOW + user_name)
+print (colorama.Fore.WHITE + "Today, We are going to Write a text into a file")
 
 # open a mylife text file
 with open("mylife.txt", "w") as my_file:
     # create a loop for y/n
     while True:
-        print ("=" * 100)
+        print (colorama.Fore.GREEN + "=" * 100)
         # ask the user to enter a line
-        user_text = input("Enter line: ")
+        user_text = input(colorama.Fore.BLUE + "Enter line: ")
         # ask the user y/n for more lines
         user_line = input("Are there more lines? (y/n) ")
         # write the user text and user line in mylife file
@@ -32,6 +33,6 @@ with open("mylife.txt", "w") as my_file:
         if user_line.lower() == "n":
             break
 
-print ("=" * 100)
-print ("Thank you for using this program.")
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
+print (colorama.Fore.YELLOW + "Thank you for using this program.")
+print (colorama.Fore.GREEN + "=" * 100)
